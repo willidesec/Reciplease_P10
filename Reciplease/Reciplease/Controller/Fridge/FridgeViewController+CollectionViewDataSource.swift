796dc4cd55bbc98cb0cad9c82405b820364a221c
William Désécot <william.desecot@hotmail.fr>
2019-03-03 14:30:42 +0100

    [FEAT]: setup food image and manage

--- Reciplease/Reciplease/Controller/Fridge/FridgeViewController+CollectionViewDataSource.swift
+++ Reciplease/Reciplease/Controller/Fridge/FridgeViewController+CollectionViewDataSource.swift
@@ -16,10 +16,20 @@ extension FridgeViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FoodCell.identifier, for: indexPath) as! FoodCell
         
+        cell.firstLetterLabel.isHidden = true
+        cell.iconImageView.isHidden = false
         
-        cell.foodLabel.text = fridge[indexPath.item]
-        cell.iconImageView.image = UIImage(named: fridge[indexPath.item])
+        let ingredient = fridge[indexPath.item]
         
+        cell.foodLabel.text = ingredient
+        cell.iconImageView.image = UIImage(named: ingredient.lowercased())
+        
+        if cell.iconImageView.image == nil {
+            cell.iconImageView.isHidden = true
+            cell.firstLetterLabel.isHidden = false
+            guard let firstLetter = ingredient.first else { return UICollectionViewCell() }
+            cell.firstLetterLabel.text = String(firstLetter).uppercased()
+        }
         return cell
     }
 }
